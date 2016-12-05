5644b9c60ae666870594d42787ec9852e4721478
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2016-12-05 11:16:47 -0500

    changes names of controllers and app

--- js/compliments/controller.js
+++ js/compliments/controller.js
@@ -1,11 +1,14 @@
 `use strict`;
 
 (function(){
-  var app = angular.module("compliments");
-  app.controller("complimentsController", ComplimentsController);
+  var app = angular.module("emergencyComp");
+  app.controller("mainController", ComplimentsController);
 
   function ComplimentsController(){
     var vm = this;
+    var randomIndex = Math.floor(Math.random() * data.length)
+    //how does this controller know what "data" is??
+    vm.data = data[randomIndex]
   }
 
 }());
